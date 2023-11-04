794257b6122756eb78bbd284f4a96294365b2aa0
Daniel-0507 <davdiu20@epoka.edu.al>
2023-11-03 21:38:00 -0400

    Starting Programming Problems

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in=new Scanner(System.in);
+        System.out.println("Please enter a number:");
+        int x=in.nextInt();
+        System.out.println("The number is"+x);
+    }
+}
